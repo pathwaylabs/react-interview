5a2cf2adff6db154cd70269a254ffff75b8134cd
Edward Guo <eguo@pathwaylabs.io>
2022-10-27 13:59:49 -0700

    defaults

---
+++ mock/defaultValues.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid } from 'uuid';
+
+const defaultValues = [
+  {
+    id: uuid(),
+    fullName: 'Harry Potter',
+    courseId: '81DDFEh8q79FsYCfNcCs',
+  },
+  {
+    id: uuid(),
+    fullName: 'Ronald Weasley',
+    courseId: '81DDFEh8q79FsYCfNcCs',
+  },
+  {
+    id: uuid(),
+    fullName: 'Hermione Granger',
+    courseId: '81DDFEh8q79FsYCfNcCs',
+  },
+  {
+    id: uuid(),
+    fullName: 'Severus Snape',
+    courseId: '81DDFEh8q79FsYCfNcCs',
+  },
+  {
+    id: uuid(),
+    fullName: 'Albus Dumbledore',
+    courseId: '81DDFEh8q79FsYCfNcCs',
+  },
+];
+
+export default defaultValues;
