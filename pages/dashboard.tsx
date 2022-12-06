6edef54e30e2d1239e0500b975159a2206413b7b
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2022-12-06 13:33:16 -0400

    feat(Dashboard): Take Home Test

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,40 +1,62 @@
-import { Avatar, Container } from '@mui/material';
-import Table, { ColConfig } from '../components/Table';
+import {Avatar, Button, Container, Input} from '@mui/material';
+import Table, {ColConfig} from '../components/Table';
 import students from '../mock/defaultValues';
-import { formatPhone } from '../utils/strings';
+import {formatPhone} from '../utils/strings';
+import Header from "../components/Header";
+import {getLocalStorageItem, setLocalStorageItem} from "../utils/localStorage";
+import BasicModal from "../components/Modal";
+import {useState} from "react";
 
 const tableConfig: ColConfig[] = [
-  {
-    key: 'id',
-    label: '',
-    render: (id) => <Avatar id={id} />
-  },
-  {
-    key: 'fullName',
-    label: 'Student Name'
-  },
-  {
-    key: 'phone',
-    label: 'Phone #',
-    transformValue: (phone: string) => formatPhone(phone)
-  }
+    {
+        key: 'id',
+        label: '',
+        render: (id) => <Avatar id={id}/>
+    },
+    {
+        key: 'fullName',
+        label: 'Student Name'
+    },
+    {
+        key: 'phone',
+        label: 'Phone #',
+        transformValue: (phone: string) => formatPhone(phone)
+    },
+    {
+        key: 'addRecord',
+        label: 'Add Attendance',
+        render: (id) => (
+            <BasicModal id={id} title={'Add Record'}/>
+        )
+    },
+    {
+        key: 'totalHours',
+        label: 'Total Hours',
+        render: (id) => {
+            const {accumulatedHours} = getLocalStorageItem(id)
+            return Number(accumulatedHours)
+        }
+    }
 ];
 
 const Dashboard = () => {
-  return (
-    <Container sx={{
-      display: 'grid',
-      placeItems: 'center',
-      width: '100vw',
-      height: '100vh',
-    }}>
-      <Table
-        rowKey="id"
-        config={tableConfig}
-        data={students}
-      />
-    </Container>
-  )
+    return (
+        <>
+            <Header/>
+            <Container sx={{
+                display: 'grid',
+                placeItems: 'center',
+                width: '100vw',
+                height: '100vh',
+            }}>
+                <Table
+                    rowKey="id"
+                    config={tableConfig}
+                    data={students}
+                />
+            </Container>
+        </>
+    )
 };
 
 export default Dashboard;
